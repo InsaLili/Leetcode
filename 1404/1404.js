e4b5d331ed66a4a4fa80a97ddb1287215f8d8670
insalili <insa.lilitong@gmail.com>
2024-05-29 21:09:24 +0200

    a mysterious carry

--- 1404/1404.js
+++ 1404/1404.js
@@ -3,30 +3,15 @@
  * @return {number}
  */
 var numSteps = function (s) {
-  let result = 0;
-  const addOne = num => {
-    for (let i = num.length - 2; i >= 0; i--) {
-      if (num.charAt(i) === '1') {
-        num = num.substring(0, i) + '0' + num.substring(i + 1);
-        if (i === 0) {
-          num = '1' + num;
-        }
-      } else {
-        num = num.substring(0, i) + '1' + num.substring(i + 1);
-        return num;
-      }
-    }
-    return num;
-  };
-  while (s.length > 1) {
-    if (s.charAt(s.length - 1) === '0') {
-      s = s.substring(0, s.length - 1);
+  let steps = 0;
+  let carry = 0;
+  for (let i = s.length - 1; i > 0; i--) {
+    if (s.charAt(i) - '0' + carry === 1) {
+      carry = 1;
+      steps += 2;
     } else {
-      s = s.slice(0, -1) + '0';
-      s = addOne(s);
+      steps++;
     }
-    result++;
   }
-
-  return result;
+  return carry + steps;
 };
