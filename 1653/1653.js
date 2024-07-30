df562738ee4d1ebd448d6889c618cd43ebed1b78
insalili <insa.lilitong@gmail.com>
2024-07-30 21:58:59 +0200

    changed to two-pass, beats 15.38%

--- 1653/1653.js
+++ 1653/1653.js
@@ -5,23 +5,17 @@
 var minimumDeletions = function (s) {
   const N = s.length;
   let count_a = new Array(N).fill(0);
-  let count_b = new Array(N).fill(0);
-
-  let b_count = 0;
-  for (let i = 0; i < N; i++) {
-    count_b[i] = b_count;
-    if (s[i] == 'b') b_count++;
-  }
-
   let a_count = 0;
   for (let i = N - 1; i >= 0; i--) {
     count_a[i] = a_count;
     if (s[i] == 'a') a_count++;
   }
 
+  let b_count = 0;
   let result = N;
   for (let i = 0; i < N; i++) {
-    result = Math.min(result, count_a[i] + count_b[i]);
+    result = Math.min(result, count_a[i] + b_count);
+    if (s[i] == 'b') b_count++;
   }
 
   return result;
