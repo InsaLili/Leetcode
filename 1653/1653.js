f982b0f187b8cb55d71432fd16f768059e6f7e2a
insalili <insa.lilitong@gmail.com>
2024-07-30 22:02:23 +0200

    change to use total a_count, beats 34.62%

--- 1653/1653.js
+++ 1653/1653.js
@@ -4,17 +4,16 @@
  */
 var minimumDeletions = function (s) {
   const N = s.length;
-  let count_a = new Array(N).fill(0);
   let a_count = 0;
   for (let i = N - 1; i >= 0; i--) {
-    count_a[i] = a_count;
     if (s[i] == 'a') a_count++;
   }
 
   let b_count = 0;
   let result = N;
   for (let i = 0; i < N; i++) {
-    result = Math.min(result, count_a[i] + b_count);
+    if (s[i] == 'a') a_count--;
+    result = Math.min(result, a_count + b_count);
     if (s[i] == 'b') b_count++;
   }
 
