63827d47ec894c1ad9954f84903d210ec57b43a8
insalili <insa.lilitong@gmail.com>
2024-07-30 22:14:55 +0200

    changed to DP, beats 76.92%

--- 1653/1653.js
+++ 1653/1653.js
@@ -4,18 +4,15 @@
  */
 var minimumDeletions = function (s) {
   const N = s.length;
-  let a_count = 0;
-  for (let i = N - 1; i >= 0; i--) {
-    if (s[i] == 'a') a_count++;
-  }
-
   let b_count = 0;
-  let result = N;
+  let min_deletions = 0;
   for (let i = 0; i < N; i++) {
-    if (s[i] == 'a') a_count--;
-    result = Math.min(result, a_count + b_count);
-    if (s[i] == 'b') b_count++;
+    if (s[i] == 'b') {
+      b_count++;
+    } else {
+      min_deletions = Math.min(min_deletions + 1, b_count);
+    }
   }
 
-  return result;
+  return min_deletions;
 };
