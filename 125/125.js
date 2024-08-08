bee9e54f20f90f4503ea72e3816d6151450b1acd
ltong <lili.tong@nl.abb.com>
2024-08-08 16:23:54 +0200

    Beats 73.29%

--- 125/125.js
+++ 125/125.js
@@ -3,14 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  const regex = /[a-z0-9]/gi;
-  const trimmed = s.match(regex);
-
-  if (!trimmed) return true;
-
-  const length = Math.ceil(trimmed.length / 2);
-  for (let i = 0; i < length; i++) {
-    if (trimmed[i].toLowerCase() !== trimmed[trimmed.length - i - 1].toLowerCase()) return false;
+  let trimmed = [];
+  for (let i of s) {
+    if (i.match(/[a-z0-9]/i)) {
+      trimmed.push(i.toLowerCase());
+    }
+  }
+  for (let i = 0; i < Math.ceil(trimmed.length / 2); i++) {
+    if (trimmed[i] !== trimmed[trimmed.length - i - 1]) return false;
   }
   return true;
 };
