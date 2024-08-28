a11b092b4ebb2085f6cb00db6b17892f93a8de86
insalili <insa.lilitong@gmail.com>
2024-08-28 22:26:08 +0200

    more efficient sliding window, Beats 86.85%

--- 424/424.js
+++ 424/424.js
@@ -4,27 +4,20 @@
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-  const letters = new Set();
-  for (let c of s) {
-    letters.add(c);
-  }
-  let maxLength = 0;
-  for (let letter of letters) {
-    let start = 0,
-      end = 0,
-      count = 0;
-    while (end < s.length) {
-      if (s[end] === letter) {
-        count++;
-      }
-      while (count + k < end - start + 1) {
-        if (s[start] === letter) count--;
-        start++;
-      }
-      maxLength = Math.max(maxLength, end - start + 1);
-      end++;
+  let start = 0,
+    end = 0,
+    maxFreq = 0;
+  const freqMap = {};
+  while (end < s.length) {
+    freqMap[s[end]] = (freqMap[s[end]] || 0) + 1;
+    maxFreq = Math.max(maxFreq, freqMap[s[end]]);
+
+    if (end + 1 - start > maxFreq + k) {
+      freqMap[s[start]]--;
+      start++;
     }
+    end++;
   }
 
-  return maxLength;
+  return end - start;
 };
