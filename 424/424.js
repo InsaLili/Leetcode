c5e76ae1e671957653c14a4c5586923ba67a9e65
insalili <insa.lilitong@gmail.com>
2024-08-28 22:09:14 +0200

    sliding window, Beats 42.12%

--- 424/424.js
+++ 424/424.js
@@ -4,35 +4,27 @@
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-  let left = 1,
-    right = s.length + 1;
-
-  const isValid = mid => {
-    const frequency = {};
-    let start = 0;
-    let max = 0;
-    for (let i = 0; i < s.length; i++) {
-      const c = s.charAt(i);
-      frequency[c] = (frequency[c] || 0) + 1;
-      if (i >= mid) {
-        frequency[s[start]] -= 1;
+  const letters = new Set();
+  for (let c of s) {
+    letters.add(c);
+  }
+  let maxLength = 0;
+  for (let letter of letters) {
+    let start = 0,
+      end = 0,
+      count = 0;
+    while (end < s.length) {
+      if (s[end] === letter) {
+        count++;
+      }
+      while (count + k < end - start + 1) {
+        if (s[start] === letter) count--;
         start++;
       }
-      max = Math.max(max, frequency[c]);
-      if (mid - max <= k) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  while (left + 1 < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (isValid(mid)) {
-      left = mid;
-    } else {
-      right = mid;
+      maxLength = Math.max(maxLength, end - start + 1);
+      end++;
     }
   }
-  return left;
+
+  return maxLength;
 };
