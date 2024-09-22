ccfa2b83346831ed3d3c9fb40865584261f9d065
insalili <insa.lilitong@gmail.com>
2024-09-22 21:22:16 +0200

    DP, Beats 18.53%

--- 42/42.js
+++ 42/42.js
@@ -5,16 +5,20 @@
 var trap = function (height) {
   let result = 0;
   const N = height.length;
+  let left_max = new Array(N).fill(0);
+  let right_max = new Array(N).fill(0);
+  left_max[0] = height[0];
+  right_max[N - 1] = height[N - 1];
   for (let i = 1; i < N - 1; i++) {
-    let left_max = 0,
-      right_max = 0;
-    for (let j = i; j >= 0; j--) {
-      left_max = Math.max(left_max, height[j]);
-    }
-    for (let j = i; j < N; j++) {
-      right_max = Math.max(right_max, height[j]);
-    }
-    result += Math.min(left_max, right_max) - height[i];
+    left_max[i] = Math.max(height[i], left_max[i - 1]);
   }
+  for (let i = N - 2; i >= 0; i--) {
+    right_max[i] = Math.max(height[i], right_max[i + 1]);
+  }
+
+  for (let i = 1; i < N - 1; i++) {
+    result += Math.min(left_max[i], right_max[i]) - height[i];
+  }
+
   return result;
 };
